ba49f414a153bb69281cd0a0a384843cd0d5fb7b
AdiRaushan <adiraushan2k@gmail.com>
2023-09-12 23:54:30 +0530

    update

--- Random Quotes js/app.js
+++
@@ -1,7 +0,0 @@
-const quote = document.querySelector(".quote");
-const writer = document.querySelector(".author");
-const btn = document.querySelector(".btn");
-
-btn.addEventListener("click", () => {
-  console.log("hello bhai");
-});
